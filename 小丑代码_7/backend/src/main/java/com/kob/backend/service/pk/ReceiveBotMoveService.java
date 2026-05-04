c43055211e3097c1dbb3d57e86abeb05768d84bf
Vader <77045149+Vader818@users.noreply.github.com>
2026-05-04 22:58:42 +0800

    删除小丑代码_7

--- 小丑代码_7/backend/src/main/java/com/kob/backend/service/pk/ReceiveBotMoveService.java
+++
@@ -1,8 +0,0 @@
-package com.kob.backend.service.pk;
-
-import org.springframework.stereotype.Service;
-
-
-public interface ReceiveBotMoveService {
-    String receiveBotMove(Integer userId, Integer direction);
-}
